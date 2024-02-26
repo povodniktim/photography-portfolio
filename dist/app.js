8dda314b1fe917e17b0558533aec0adb49e3bd77
Tim Povodnik <tim.povodnik@gmail.com>
2024-02-26 01:34:04 +0100

    Nika Komljenovic pictures addded

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      console.log(entry.target); // Log the observed element
-      console.log(entry.isIntersecting); // Log whether the element is intersecting
+      console.log(entry.target);
+      console.log(entry.isIntersecting);
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
-        observer.unobserve(entry.target);
       } else {
         entry.target.classList.remove("show");
       }
@@ -18,10 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
   // Adding scroll functionality after the page has loaded
   window.addEventListener("load", function () {
     const scrollButton = document.getElementById("scrollDownButton");
-    const targetSection = document.getElementById("scrollBtnTarget"); // Replace 'targetSection' with the ID of your target section
+    const targetSection = document.getElementById("scrollBtnTarget");
 
     scrollButton.addEventListener("click", function () {
       targetSection.scrollIntoView({ behavior: "smooth" });
+    });
+
+    // Adjust scroll behavior to navigate to the full page content
+    const navLinks = document.querySelectorAll('a[href^="#"]');
+    navLinks.forEach((link) => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      });
     });
   });
 });
